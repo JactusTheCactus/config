285f4266c90a774f89043c6a0ebe9cec1f09d625
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2026-06-04 03:11:50 -0700

    -

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -2,4 +2,9 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 config.font_size = 20
 config.font = wezterm.font 'Fira Code'
+local act_rel = wezterm.action.ActivateTabRelative
+config.keys = {
+	{ key = 'LeftArrow', mods = 'CTRL', action = act_rel(-1) },
+	{ key = 'RightArrow', mods = 'CTRL', action = act_rel(1) }
+}
 return config
