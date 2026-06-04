8e9a92628376cf19207524107e95024d761253ae
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2026-06-04 03:20:04 -0700

    -

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -2,9 +2,11 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 config.font_size = 20
 config.font = wezterm.font 'Fira Code'
-local act_rel = wezterm.action.ActivateTabRelative
+local act = wezterm.action
+local act_rel = act.ActivateTabRelative
 config.keys = {
 	{ key = 'LeftArrow', mods = 'CTRL', action = act_rel(-1) },
-	{ key = 'RightArrow', mods = 'CTRL', action = act_rel(1) }
+	{ key = 'RightArrow', mods = 'CTRL', action = act_rel(1) },
+	{ key = 't', mods = 'CTRL', action = act.SpawnTab 'CurrentPaneDomain' }
 }
 return config
