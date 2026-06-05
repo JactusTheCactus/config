325d67c0c6b323ab7b0db09c6d109d49ff6c49a8
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2026-06-05 01:18:50 -0700

    -

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -49,7 +49,7 @@ return merge(wezterm.config_builder(), {
 			horizontal_align = 'Center',
 			attachment = { Parallax = 0.1 },
 			repeat_y = 'Repeat',
-			repeat_y_size = '75%'
+			repeat_y_size = '87.5%'
 		}
 	},
 	clean_exit_codes = { 130 },
