dd52ca2111842a85a89b359d61b7c7af2fdce941
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2026-06-05 01:56:38 -0700

    -

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -49,6 +49,7 @@ return merge(wezterm.config_builder(), {
 	clean_exit_codes = { 130 },
 	colors = {
 		scrollbar_thumb = '#666',
+		visual_bell = '#666',
 	},
 	font = wezterm.font 'Fira Code',
 	font_size = 20,
