9cdafab224dae80a95653c337c4e0c285052d163
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2026-06-05 02:15:34 -0700

    -

--- private_dot_config/wezterm/empty_helpers.lua
+++ private_dot_config/wezterm/empty_helpers.lua
@@ -1,1 +1,9 @@
-local helpers = {}
+return {
+	all = function(conditions)
+		for _, v in ipairs(conditions)
+			do if not v then return false end
+		end
+		return true
+	end,
+	--merge = function(defaul)
+}
