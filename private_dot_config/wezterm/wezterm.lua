23bde3dcd7f47996cf96f70abda27611172a2b4a
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2026-06-04 20:27:50 -0700

    -

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ config.keys = {
 	{ key = 'RightArrow', mods = 'CTRL', action = act_rel(1) },
 	{ key = 't', mods = 'CTRL', action = act.SpawnTab 'CurrentPaneDomain' }
 }
-config.status_update_interval = 250 -- milliseconds
+config.status_update_interval = 100 -- milliseconds
 wezterm.on("update-status", function(window, pane)
 	local overrides = window:get_config_overrides() or {}
 	local dimensions = pane:get_dimensions()
