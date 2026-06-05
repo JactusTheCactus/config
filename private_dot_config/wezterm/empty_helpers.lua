28b0daabd2d491f0233e4337488cf37ce8945aeb
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2026-06-05 02:22:54 -0700

    -

--- private_dot_config/wezterm/empty_helpers.lua
+++ private_dot_config/wezterm/empty_helpers.lua
@@ -1,7 +1,9 @@
 local helpers = {}
 function helpers.all(conditions)
-	for _, v in ipairs(conditions)
-		do if not v then return false end
+	for _, v in ipairs(conditions) do
+		if not v then
+			return false
+		end
 	end
 	return true
 end
