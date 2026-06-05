2846e2fbd38b36d66ae239b51f1f74fd364e9873
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2026-06-05 00:56:49 -0700

    -

--- private_dot_config/wezterm/wezterm.lua
+++ private_dot_config/wezterm/wezterm.lua
@@ -49,8 +49,10 @@ wezterm.on("update-status", function(window, pane)
 	window:set_config_overrides(overrides)
 end)
 config.background = merge(config.background, {
-	source = { File = '/usr/share/backgrounds/ubuntu-default-greyscale-wallpaper.png' },
-	opacity = 0.5
+	{
+		source = { File = '/usr/share/backgrounds/ubuntu-default-greyscale-wallpaper.png' },
+		opacity = 0.5
+	}
 })
 config.clean_exit_codes = { 0, 130 }
 return config
