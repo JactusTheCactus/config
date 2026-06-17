58d266855b8b65b824a8d406aea2577134021fe4
JactusTheCactus (PC) <threateninglyamusing@gmail.com>
2026-06-17 00:29:57 -0700

    -

--- private_dot_config/nixos/configuration.nix
+++ private_dot_config/nixos/configuration.nix
@@ -3,16 +3,30 @@
 		./hardware-configuration.nix
 		(if builtins.pathExists ./local.nix then ./local.nix else {})
 	];
-	boot.loader.grub.enable = true;
-	boot.loader.grub.device = "/dev/sda";
-	boot.loader.grub.useOSProber = true;
-	networking.networkmanager.enable = true;
-	networking.firewall.allowedTCPPorts = [22];
+	boot.loader.grub = {
+		enable = true;
+		device = "/dev/sda";
+		useOSProber = true;
+	};
+	networking = {
+		networkmanager.enable = true;
+		firewall.allowedTCPPorts = [22];
+	};
 	time.timeZone = "America/Vancouver";
 	i18n.defaultLocale = "en_GB.UTF-8";
-	services.xserver.xkb = {
-		layout = "us";
-		variant = "";
+	services = {
+		greetd = {
+			enable = true;
+			settings.default_session = {
+				user = "greeter";
+				command = with pkgs; "${tuigreet}/bin/tuigreet --time --cmd Hyprland";
+			};
+		};
+		openssh.enable = true;
+		xserver.xkb = {
+			layout = "us";
+			variant = "";
+		};
 	};
 	users.users.devin = {
 		isNormalUser = true;
@@ -24,61 +38,69 @@
 		packages = with pkgs; [];
 		shell = pkgs.nushell;
 	};
-	environment.shells = with pkgs; [nushell];
-	environment.variables = {
-		TERMINAL = "wezterm";
-	};
 	nixpkgs.config.allowUnfree = true;
 	nix.settings.experimental-features = [
 		"nix-command"
 		"flakes"
 	];
 	fonts.packages = with pkgs; [fira-code];
-	environment.systemPackages = with pkgs; [
-		bat
-		chezmoi
-		cowsay
-		dasel
-		git
-		gum
-		helix
-		hyprpaper
-		lazygit
-		libreoffice-qt
-		lolcat
-		lowdown
-		mc
-		minijinja
-		neovim
-		nushell
-		shellcheck
-		tree
-		vhs
-		wezterm
-		waybar
-		wofi
-		yq
-		(pkgs.writeShellApplication {
-			name = "ns";
-			runtimeInputs = with pkgs; [fzf nix-search-tv];
-			text = builtins.readFile "${pkgs.nix-search-tv.src}/nixpkgs.sh";
-			excludeShellChecks = ["SC2016"];
-		})
-	];
-	programs.hyprland.enable = true;
-	programs.steam.enable = true;
-	xdg.portal.enable = true;
-	xdg.portal.extraPortals = with pkgs; [xdg-desktop-portal-gtk];
-	services.greetd = {
+	environment = {
+		shells = with pkgs; [nushell];
+		variables = {
+			TERMINAL = "wezterm";
+		};
+		systemPackages = with pkgs; [
+			bat
+			chezmoi
+			cowsay
+			dasel
+			git
+			gum
+			helix
+			hyprpaper
+			lazygit
+			libreoffice-qt
+			lolcat
+			lowdown
+			mc
+			minijinja
+			neovim
+			nushell
+			shellcheck
+			tree
+			vhs
+			wezterm
+			waybar
+			wofi
+			yq
+			(pkgs.writeShellApplication {
+				name = "ns";
+				runtimeInputs = with pkgs; [fzf nix-search-tv];
+				text = builtins.readFile "${pkgs.nix-search-tv.src}/nixpkgs.sh";
+				excludeShellChecks = ["SC2016"];
+			})
+		];
+	};
+	programs = {
+		hyprland.enable = true;
+		# steam.enable = true;
+	};
+	xdg.portal = {
 		enable = true;
-		settings = {
-			default_session = {
-				user = "greeter";
-				command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+		extraPortals = with pkgs; [xdg-desktop-portal-gtk];
+	};
+	services = {
+		greetd = {
+			enable = true;
+			settings = {
+				default_session = {
+					user = "greeter";
+					command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+				};
 			};
 		};
+		openssh.enable = true;
 	};
 	hardware.graphics.enable = true;
-	services.openssh.enable = true;
 	system.stateVersion = "25.11";
 }
